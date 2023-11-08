1adc6d2791fd56702249459dd15c313b1aa03e5e
ThinkingData <sdk@thinkingdata.cn>
2023-11-08 10:25:21 +0800

    - 优化代码- 升级 iOS SDK

--- ThinkingData/RNThinkingData.podspec
+++ ThinkingData/RNThinkingData.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name         = "RNThinkingData"
-  s.version      = "2.4.6"
+  s.version      = "3.0.0"
   s.summary      = "RNThinkingData"
   s.description  = <<-DESC
                   RNThinkingData
--- ThinkingData/ThinkingAnalyticsAPI.js
+++ ThinkingData/ThinkingAnalyticsAPI.js
@@ -6,7 +6,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const { RNThinkingAnalyticsModule } = NativeModules;
 
-const TESDKVERSION = '2.4.6';
+const TESDKVERSION = '3.0.0';
 
 const AutoTrackEventType = {
     APP_START: 'appStart',
